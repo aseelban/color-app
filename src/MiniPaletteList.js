264d47fabc0c85499b7c345cde2841a9d0e1a18c
aseelban <aseelban@gmail.com>
2020-07-02 03:31:48 +0300

    start styling MiniPalette

--- src/MiniPaletteList.js
+++ src/MiniPaletteList.js
@@ -14,7 +14,11 @@ const styles = {
         }
     },
     colors: {
-        background: 'grey',
+        background: '#e9e9e9',
+        height: '150px',
+        width: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden'
     },
     title: {
         display: 'flex',
@@ -30,17 +34,30 @@ const styles = {
     emoji: {
         marginLeft: "0.5rem",
         fontSize: '1.5rem'
+    },
+    miniColorBox: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: '0 auto',
+        position: 'relative',
+        marginTop: '-3.5px'
     }
 }
 
 function MiniPaletteList(props) {
 
-    const {classes,paletteName,emoji} = props;
+    const {classes,paletteName,emoji,colors} = props;
+    const miniColorBox = colors.map((color) => {
+         return <div className={classes.miniColorBox} style={{backgroundColor: color.color}} key={color.name}></div>
+    })
 
     return (
         <div>
             <div className={classes.root}>
-                <div className={classes.colors} />
+                <div className={classes.colors}>
+                    {miniColorBox}
+                </div>
                 <h4 className={classes.title}>
                     {paletteName} <span className={classes.emoji}>{emoji}</span>
                 </h4>  
