7e9c45fb1fd1854fb7e71f39552e0c8474218ae1
aseelban <aseelban@gmail.com>
2020-07-28 06:14:36 +0300

    style DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
+
 
 const styles = {
   root: {
@@ -9,14 +11,44 @@ const styles = {
     display: "inline-block",
     position: "relative",
     marginBottom: "-3.5px",
-    "&:hover button": {
-      transition: "0.5s",
+    // change the removeIcon color on hover
+    "&:hover svg": {
+      color: "#fff",
+      transform: "scale(1.3)"
     },
   },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: 0,
+    bottom: 0,
+    padding: 10,
+    color: "rgba(0,0,0,0.5)",
+    letterSpacing: 1,
+    textTransform: "uppercase",
+    fontSize: 12,
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  removeIcon: {
+    color: "rgba(0,0,0,0.5)",
+    cursor:'pointer',
+    transition: "all 0.3s ease-in-out"
+    
+  }
 };
 
 const DraggableColorBox = (props) => {
-  return <div className={props.classes.root} style={{ background: props.color }}>{props.name}</div>;
+  const { classes } = props;
+
+  return (
+    <div className={props.classes.root} style={{ background: props.color }}>
+      <div className={classes.boxContent}>
+        <span>{props.name}</span>
+        <DeleteSharpIcon  className={classes.removeIcon} />
+      </div>
+    </div>
+  );
 };
 
 export default withStyles(styles)(DraggableColorBox);
